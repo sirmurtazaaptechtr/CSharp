4d6085fd6847660ec10139152ec90f1aa4c461a3
Syed Hussain <sirmurtazaaptechtr@outlook.com>
2023-03-11 13:03:53 +0500

    11-Mar-2023

---
+++ Mar112023-1/Mar112023-1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace Mar112023_1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int sum;
+            Program.Add(2, 3);
+            Program.Add(2.5f, 3.7f);
+            Program.Add(2, 3, 4);
+            Program.Add(2, 4, out sum);
+        }
+        public static void Add(int FN, int SN)
+        {
+            Console.WriteLine("1:Sum = {0}", FN + SN);
+        }
+        public static void Add(float FN, float SN)
+        {
+            Console.WriteLine("2:Sum = {0}", FN + SN);
+        }
+        public static void Add(int FN, int SN, int TN)
+        {
+            Console.WriteLine("3:Sum = {0}", FN + SN + TN);
+        }
+        public static void Add(int FN, int SN, out int Sum)
+        {
+            Console.WriteLine("4:Sum = {0}", FN + SN);
+            Sum = FN + SN;
+        }
+        //public static int Add(int FN, int SN)
+        //{
+        // we cant overload methods on the basis of return type
+        // as they are not the part of signature
+        //}
+    }
+}
