f6c9969b09ba96da6e49f1c60bcea28f6a1efdab
Syed Hussain <sirmurtazaaptechtr@outlook.com>
2023-03-16 15:04:56 +0500

    16-Mar-2023

---
+++ Mar162023-1/Mar162023-1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace Mar162023_1
+{
+    interface IMyInterface
+    {
+        void myMethod();  
+    }
+    interface IEmployee
+    {
+        void DisplayEmp();
+    }
+    interface ICustomer : IMyInterface
+    {
+        string Name { get; }
+        void Print();
+    }
+    public class Customer : ICustomer, IEmployee
+    {
+        public string Name { get; set; }
+        public void Print()
+        {
+            Console.WriteLine("This is Implementation of Print Method of Interface");
+        }
+        void IEmployee.DisplayEmp()
+        {
+            throw new NotImplementedException();
+        }
+        void IMyInterface.myMethod()
+        {
+            throw new NotImplementedException();
+        }        
+    }
+    internal class Program
+    {
+        static void Main()
+        {
+            Customer c1 = new Customer();
+            c1.Name = "Sardar Uzair";
+
+            ICustomer customer = new Customer();
+            customer.Print();
+            Console.WriteLine(c1.Name);
+
+        }
+    }
+}
