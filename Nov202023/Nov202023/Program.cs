acc5b1d5738b74eb8fd1d50d64eabecc8dbbe679
Syed Murtaza Hussain <sirmurtazaaptechtr@outlook.com>
2023-11-20 16:56:04 +0500

    20-Nov-2023

---
+++ Nov202023/Nov202023/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Security.Permissions;
+
+namespace Nov202023
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] students = new string[6];
+            students[0] = "Ahsan";
+            students[1] = "Kinza";
+            students[2] = "Yaseen";
+            students[3] = "Rohan";
+            students[4] = "Saqlain";
+            students[5] = "Saqlain";
+            //for(int i = 0; i < students.Length; i++) 
+            //{
+            //    Console.WriteLine("Hi " + students[i]);
+            //}
+            foreach (string student in students)
+            {
+                Console.WriteLine("Hi " + student);
+            }
+            Program pro = new Program();
+
+            Console.WriteLine("Even Numbers between 1 and 10");
+            pro.EvenNumbers(1, 10);
+
+            Console.WriteLine("Even Numbers between 5 and 15");
+            pro.EvenNumbers(5, 15);
+
+            int sq = Program.Squrer(12);
+
+            Console.WriteLine("The square of 12 is {0}", sq);
+
+            Console.WriteLine("The square of 5 is {0}", Program.Squrer(5));
+
+        }
+
+        public void EvenNumbers(int start_num,int end_num)
+        {
+            for(int i = start_num; i <= end_num; i++)            
+            {
+                if(i%2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        public static int Squrer (int number)
+        {
+            return number * number;
+        }
+    }
+}
---
+++ Nov202023_1/Nov202023_1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace Nov202023_1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int a = 10; int b = 20;
+
+            Program p = new Program(); // creating instance variable 'p' of 'Program' class
+
+            Console.WriteLine("Call by Value Example");
+            Console.WriteLine("Outside BS : (a, b)--->({0}, {1})", a, b);
+            p.Swap1(a, b); // call by value
+            Console.WriteLine("Outside AS : (a, b)--->({0}, {1})", a, b);
+
+            Console.WriteLine("Call by Reference Example");
+            Console.WriteLine("Outside BS : (a, b)--->({0}, {1})", a, b);
+            p.Swap2(ref a, ref b);// call by reference
+            Console.WriteLine("Outside AS : (a, b)--->({0}, {1})", a, b);
+        }
+        public void Swap1(int x, int y) 
+        {
+            Console.WriteLine("Inside BS : (x, y)--->({0}, {1})", x, y);
+            int temp;
+            temp = x;
+            x = y;
+            y = temp;
+            Console.WriteLine("Inside AS : (x, y)--->({0}, {1})", x, y);
+        }
+        public void Swap2(ref int x, ref int y)
+        {
+            Console.WriteLine("Inside BS : (x, y)--->({0}, {1})", x, y);
+            int temp;
+            temp = x;
+            x = y;
+            y = temp;
+            Console.WriteLine("Inside AS : (x, y)--->({0}, {1})", x, y);
+        }
+    }
+}
