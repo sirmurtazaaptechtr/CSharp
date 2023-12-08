3a3b4ea813380fecac11ccc935ad5b75249f8387
Syed Murtaza Hussain <sirmurtazaaptechtr@outlook.com>
2023-12-08 17:10:39 +0500

    08-Dec-2023

---
+++ Dec072023/Dec072023/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+namespace Dec072023
+{
+    interface IStudent
+    {
+        void Show();
+        
+    }
+    interface I2
+    {
+        void I2Method();
+    }
+    public struct Student : IStudent, I2
+    {
+        private int _id;
+        private string _name;        
+        public Student (int ID, string Name)
+        {
+            this._id = ID;
+            this._name = Name;
+        }
+
+        public int Id { get => this._id; set => this._id = value; }
+        public string Name { get => this._name; set => this._name = value; }
+
+        public void Print ()
+        {
+            Console.WriteLine("ID : {0}\nName : {1}\n", Id, Name);
+        }
+        public void Show ()
+        {
+
+        }
+
+        void I2.I2Method()
+        {
+            
+        }
+    }
+    internal class Program
+    {
+        static void Main()
+        {
+            Student student1 = new Student(1, "Khurram");
+            student1.Name = "Saad Khurram";
+            Student student2 = new Student(2, "Taha");
+            student2.Name = "Taha Tehseen";
+
+            Student student3 = new Student
+            {
+                Name = "Ali Raza",
+                Id = 3
+            };
+
+            student1.Print();
+            student2.Print();
+            student3.Print();
+        }
+    }
+}
---
+++ Dec082023/Dec082023/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Dec082023
+{
+    interface I1
+    {
+        void Show();
+        void Input (string name);
+       
+    }
+    interface I2
+    {
+        void I2Method();
+    }
+    public struct Student : I1, I2
+    {
+        public int Id {  get; set; }
+        public string Name { get; set; }
+
+        public Student (int id, string name) 
+        {
+            Id = id;
+            Name = name;
+        }
+        public void Show()
+        {
+
+        }
+        public void Input (string name)
+        {
+            Name = name;
+        }
+        public void I2Method() 
+        {
+
+        }
+
+        public void Print () 
+        {
+            Console.WriteLine("ID : {0}\nName : {1}\n", Id, Name);
+        }
+    }
+    internal class Program
+    {
+        static void Main()
+        {
+            Student std1 = new Student();
+            Student std2 = new Student();
+            Student std3 = new Student(3, "Ahsan Khan");
+            Student std4 = new Student 
+            { 
+                Id = 4, 
+                Name = "Saqlain Dejwani"
+            };
+            Student std5 = new Student();
+
+            std1.Id = 1;
+            std1.Name = "Aptech Learning";
+            std2.Id = 2;
+            std2.Name = "Kinza Danish";
+
+            std5.Id = 5;
+            std5.Input("Ali Raza");
+
+
+            std1.Print();
+            std2.Print();
+            std3.Print();
+            std4.Print();
+            std5.Print();
+        }
+    }
+}
