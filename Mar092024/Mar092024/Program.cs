21b24e0cf939277eedcb2b1ec77c3420fea59171
Syed Murtaza Hussain <sirmurtazaaptechtr@outlook.com>
2024-03-11 18:51:19 +0500

    11-Mar-2024

---
+++ Mar092024/Mar092024/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Mar092024
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int validAge = 18;
+            Console.Write("Enter Your Age : ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if(age >= validAge) 
+            {
+                Console.WriteLine("You are Eligible to get a Driving License");
+            }
+            else
+            {
+                Console.WriteLine($"You will be Eligible after {validAge - age} years.");
+            }
+
+            double phy, che, mat, eng, urd, maxmarks = 125, obtmarks, percentage;
+            Console.Write("Physics Marks : ");
+            phy = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Chemistry Marks : ");
+            che = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Maths Marks : ");
+            mat = Convert.ToInt32(Console.ReadLine());
+            Console.Write("English Marks : ");
+            eng = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Urdu Marks : ");
+            urd = Convert.ToInt32(Console.ReadLine());
+
+            obtmarks = phy + che + mat + eng + urd;
+
+            percentage = Math.Round((obtmarks/maxmarks) * 100,2);
+
+            Console.WriteLine($"Marks : {obtmarks}/{maxmarks}\nPercentage : {percentage}%");
+
+            if(percentage < 40)
+            {
+                Console.WriteLine("Status : PASS");
+            }
+            else
+            {
+                Console.WriteLine("Status : FAIL");
+            }
+
+
+        }
+    }
+}
---
+++ Mar112024/Mar112024/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Globalization;
+
+namespace Mar112024
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            float num1, num2;
+            
+            Console.Write("Enter First Number : ");
+            num1 = float.Parse(Console.ReadLine());
+
+            Console.Write("Enter Second Number : ");
+            num2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+            Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
+            Console.WriteLine($"{num1} x {num2} = {num1 * num2}");
+            Console.WriteLine($"{num1} / {num2} = {num1 / num2}");
+            Console.WriteLine($"{num1} % {num2} = {num1 % num2}");
+            Console.WriteLine($"{num1} ^ {num2} = {Math.Pow(num1,num2)}");
+
+            Console.WriteLine($"num1 = {num1}");
+            
+            Console.WriteLine($"{num1}++ = {num1++}");
+            Console.WriteLine($"num1 = {num1}");
+
+            Console.WriteLine($"++{num1} = {++num1}");
+            Console.WriteLine($"num1 = {num1}");
+
+            Console.WriteLine($"{num1}-- = {num1--}");
+            Console.WriteLine($"num1 = {num1}");
+
+            Console.WriteLine($"--{num1} = {--num1}");
+            Console.WriteLine($"num1 = {num1}");
+
+            num1 += 2;
+            Console.WriteLine($"num1+=2 = {num1}");
+            num1 -= 2;
+            Console.WriteLine($"num1-=2 = {num1}");
+            num1 *= 2;
+            Console.WriteLine($"num1*=2 = {num1}");
+            num1 /= 2;
+            Console.WriteLine($"num1/=2 = {num1}");
+            num1 %= 2;
+            Console.WriteLine($"num1%=2 = {num1}");
+
+            Console.WriteLine($"num1 = {num1}, num2 = {num2}");
+            Console.WriteLine($"num1({num1}) == num2({num2}) => {num1 == num2}");
+            Console.WriteLine($"num1({num1}) != num2({num2}) => {num1 != num2}");
+            Console.WriteLine($"num1({num1}) < num2({num2}) => {num1 < num2}");
+            Console.WriteLine($"num1({num1}) > num2({num2}) => {num1 > num2}");
+            Console.WriteLine($"num1({num1}) <= num2({num2}) => {num1 <= num2}");
+            Console.WriteLine($"num1({num1}) >= num2({num2}) => {num1 >= num2}");
+
+            double pi = 22.0d / 7;
+            Console.WriteLine(pi);
+            Console.WriteLine(Math.Round(pi));
+            Console.WriteLine(Math.Round(pi,2));
+
+            string text = "Moiz ur Rehman";
+            Console.WriteLine($"\"{text}\" has {text.Length} characters in it.");
+            Console.WriteLine($"\"{text.ToLower()}\"");
+            Console.WriteLine($"\"{text.ToUpper()}\"");
+
+            string firstName = "Ahmed";
+            string lastName = "Raza";
+            //string fullName = firstName + " " + lastName;
+            //string fullName = string.Concat(firstName, " ", lastName);
+            string fullName = $"{firstName} {lastName}";
+
+            Console.WriteLine(fullName);
+        }
+    }
+}
