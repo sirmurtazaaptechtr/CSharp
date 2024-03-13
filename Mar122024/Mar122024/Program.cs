8af4be461617c53653fcb3533821e95420620f1c
Syed Murtaza Hussain <sirmurtazaaptechtr@outlook.com>
2024-03-13 16:36:09 +0500

    13-Mar-2024

---
+++ Mar122024/Mar122024/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Mar122024
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            float num1, num2, res;
+            int op;
+
+            Console.Write("Enter First Number : ");
+            num1 = float.Parse(Console.ReadLine());
+            Console.Write("Enter Second Number : ");
+            num2 = float.Parse(Console.ReadLine());
+
+            Console.Write("1. Addition\n2. Subtraction\n3. Multiplicaton\n4. Division\nEnter Your Choice(1-4) : ");
+            op = int.Parse(Console.ReadLine());
+
+            switch(op)
+            {
+                case 1:
+                    res = num1 + num2;
+                    Console.WriteLine($"{num1} + {num2} = {res}");
+                break;
+                case 2:
+                    res = num1 - num2;
+                    Console.WriteLine($"{num1} - {num2} = {res}");
+                break;
+                case 3:
+                    res = num1 * num2;
+                    Console.WriteLine($"{num1} X {num2} = {res}");
+                break;
+                case 4:
+                    res = num1 / num2;
+                    Console.WriteLine($"{num1} / {num2} = {res}");
+                break;
+                default:
+                    Console.WriteLine("Invalid Choice!");
+                break;
+            }
+        }
+    }
+}
---
+++ Mar132024/Mar132024/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Mar132024
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string fullName = "", firstName = "", midName = "", lastName = "";
+            char ch = ' ';
+
+            Console.Write("Enter Full Name : ");
+            fullName = Console.ReadLine();
+
+            //Console.WriteLine($"\"{fullName}\" has {fullName.Length} characters in it.");
+            //Console.WriteLine(fullName[6]);
+            //Console.WriteLine(fullName.IndexOf(ch));
+            //Console.WriteLine(fullName.IndexOf(ch, fullName.IndexOf(ch) + 1));
+
+            //firstName = fullName.Substring(0, fullName.IndexOf(ch));
+            //midName = fullName.Substring(fullName.IndexOf(ch) + 1, fullName.IndexOf(ch, fullName.IndexOf(ch) + 1) - fullName.IndexOf(ch));
+            //lastName = fullName.Substring(fullName.IndexOf(ch, fullName.IndexOf(ch) + 1)+1);
+
+            //Console.WriteLine($"Full Name : {fullName}");
+            //Console.WriteLine($"First Name : {firstName}");
+            //Console.WriteLine($"Middle Name : {midName}");
+            //Console.WriteLine($"Last Name : {lastName}");
+
+            string[] nameParts = fullName.Split(ch);
+
+            int len = nameParts.Length;
+
+            switch(len)
+            {
+                case 1:
+                    firstName = nameParts[0];
+                break;
+                case 2:
+                    firstName = nameParts[0];
+                    lastName = nameParts[1];
+                break;
+                case 3:
+                    firstName = nameParts[0];
+                    midName = nameParts[1];
+                    lastName = nameParts[2];
+                break;
+            }            
+
+            Console.WriteLine($"First Name : {firstName}");
+            Console.WriteLine($"Middle Name : {midName}");
+            Console.WriteLine($"Last Name : {lastName}");
+        }
+    }
+}
