c764ba81ea73987097eade635a93dfa0a5caccfe
Syed Murtaza Hussain <sirmurtazaaptechtr@outlook.com>
2024-03-15 16:30:36 +0500

    15-Mar-2024

---
+++ Mar142024/Mar142024/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Mar142024
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int num = 10;
+            while(num < 10)
+            {
+                Console.WriteLine($"{num} is less than 10");
+                num ++;
+            }
+            Console.WriteLine($"num = {num}");
+
+
+            num = 10;
+            do
+            {
+                Console.WriteLine($"{num} is less than 10");
+                num++;
+            }
+            while (num < 10);
+            Console.WriteLine($"num = {num}");
+
+            int st, ed, cnt;
+            Console.Write("Enter Start : ");
+            st = int.Parse(Console.ReadLine());
+            Console.Write("Enter End : ");
+            ed = int.Parse(Console.ReadLine());
+            cnt = st;
+
+            if(cnt < ed)
+            {                
+                while (cnt <= ed) 
+                {
+                    Console.WriteLine(cnt);
+                    cnt++;
+                }
+            }
+            else
+            {             
+                while (cnt >= ed)
+                {
+                    Console.WriteLine(cnt);
+                    cnt--;
+                }
+
+            }
+
+        }
+    }
+}
---
+++ Mar152024/Mar152024/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Mar152024
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int number,remainder,divisor;
+            bool isPrime = true;
+            Console.Write("Enter any Number : ");
+            number = int.Parse(Console.ReadLine());
+            // For Even and ODD
+            remainder = number % 2;
+
+            if(remainder == 0)
+            {
+                Console.WriteLine($"{number} is Even");
+            }
+            else
+            {
+                Console.WriteLine($"{number} is Odd");
+            }
+
+            // For PRIME NUMBER
+            divisor = 2;
+            while(divisor < number)
+            {
+                if(number % divisor == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+                divisor++;
+            }
+
+            if(isPrime)
+            {
+                Console.WriteLine($"{number} is PRIME");
+            }
+            else
+            { 
+                Console.WriteLine($"{number} is Not-PRIME"); 
+            }
+
+            //float marks,percentage;
+            //Console.Write("Enter Obtained Marks Out of 75 : ");
+            //marks = float.Parse(Console.ReadLine());
+
+            //percentage = (float)Math.Round(marks / 75 * 100,2);
+            //Console.WriteLine($"Percentage : {percentage}%");
+
+            //if (percentage >= 80)
+            //{
+            //    Console.WriteLine("Grade : A+");
+            //}
+            //else if(percentage >= 70)
+            //{
+            //    Console.WriteLine("Grade : A");
+            //}
+            //else if (percentage >= 60)
+            //{
+            //    Console.WriteLine("Grade : B");
+            //}
+            //else if (percentage >= 50)
+            //{
+            //    Console.WriteLine("Grade : C");
+            //}
+            //else if (percentage >= 40)
+            //{
+            //    Console.WriteLine("Grade : D");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Grade : No-Grade");
+            //}
+        }
+    }
+}
