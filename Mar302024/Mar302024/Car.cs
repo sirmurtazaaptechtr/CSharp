586a80808075ce6868003477ce1845e965f6717f
Syed Murtaza Hussain <sirmurtazaaptechtr@outlook.com>
2024-03-30 15:04:31 +0500

    30-Mar-2024

---
+++ Mar302024/Mar302024/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Mar302024
+{
+    internal class Car
+    {
+        // Class members
+        // Fields
+        public static string CompanyName = "Ahmed Motors";// static field
+        public string color;        // field
+        public int maxSpeed;        // field
+
+        // Methods
+        public void fullThrottle()   // method
+        {
+            Console.WriteLine("The car is going as fast as it can!");
+        }
+        public void CarInfor()  // method
+        {
+            Console.WriteLine($"Welcome to {CompanyName}\nThe color of my car is {this.color} and it's max speed is {this.maxSpeed}km/hr");
+        }
+
+        public static void ClassInfo () // static method
+        {
+            Console.WriteLine("This is Car Class");
+        }
+
+    }
+}
---
+++ Mar302024/Mar302024/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Mar302024
+{
+    
+    internal class Program
+    {
+        static void Main()
+        {
+            Car c1 = new Car();
+            c1.color = "red";
+            c1.maxSpeed = 180;
+
+            Car c2 = new Car();
+            c2.color = "blue";
+            c2.maxSpeed = 250;
+
+            c1.CarInfor();
+            c2.CarInfor();            
+
+            Car.ClassInfo();
+            Console.WriteLine($"Company Name : {Car.CompanyName} ");
+        }
+    }
+}
