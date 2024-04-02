16470913bd450f03f21598ce31729d387a1aa2be
Syed Murtaza Hussain <sirmurtazaaptechtr@outlook.com>
2024-04-02 15:07:32 +0500

    02-Apr-2024

---
+++ Apr012024/Apr012024/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Apr012024
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            for(int i = 1;i < 10 ;i++) 
+            {
+                if(i % 2 == 0)
+                {
+                    continue;
+                    //break;
+                }
+                Console.WriteLine(i);
+            }
+
+            //int[] number = { 15, 20 };
+
+            //int[] number;
+            //number = new int[] { 15, 20 };
+
+            int[] numbers;
+            numbers = new int[5] { 15, 20, 45, 23, 11 };
+            //--------------------------For loop--------------------------
+            //for(int i = 0; i < numbers.Length; i++) 
+            //{
+            //    Console.WriteLine(numbers[i]);
+            //}
+            //--------------------------While loop--------------------------
+            //int i = 0;
+
+            //while (i < numbers.Length) 
+            //{
+            //    Console.WriteLine(numbers[i]);
+            //    i++;
+            //}
+            foreach (int number in numbers) 
+            {
+                Console.WriteLine(number);
+            }
+            
+            
+        }
+    }
+}
---
+++ Apr022024/Apr022024/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Apr022024
+{
+    class Car
+    {
+        public string regNumber;
+        public string model;
+        public string color;
+        public string year;
+
+        //public Car() {}
+
+        public Car (string RegNum)
+        {
+            this.regNumber = RegNum;
+        }
+        public Car(string RegNum,string model,string year)
+        {
+            this.regNumber = RegNum;
+            this.model = model;
+            this.year = year;
+        }
+
+        public void ShowCar ()
+        {
+            Console.WriteLine($"Registration Number : {this.regNumber}\nModel : {this.model}\nColor : {this.color}\nYear : {this.year}");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main()
+        {
+            Car c1 = new Car("ABC-123");
+            Car c2 = new Car("BGZ-423","WaganR","2016");
+            c2.color = "White";
+            Car c3 = new Car("XYZ-987","Civic","2019");
+            
+            c2.ShowCar();
+        }
+    }
+}
