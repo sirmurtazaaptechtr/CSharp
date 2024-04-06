b70d1cc097b79cec4c0c2e2aad6c02b5178bc1cf
Syed Murtaza Hussain <sirmurtazaaptechtr@outlook.com>
2024-04-06 15:08:11 +0500

    06-Apr-2024

---
+++ Apr062024-2/Apr062024-2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Security.AccessControl;
+
+namespace Apr062024_2
+{
+    class Student
+    {
+        //fields
+        private int _id;
+        private string _name;
+        private string _email;
+        //properties
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+        //Automatic Properties (Short Hand) 
+        public string Phone { get; set; }
+    }
+    internal class Program
+    {
+        
+        static void Main()
+        {
+            Student s1 = new Student();
+            s1.Id = 1;
+            s1.Name = "Anshara Rizwan";
+            s1.Email = "anshararizwan48@gmail.com";
+            s1.Phone = "0300-1234567";
+
+            Console.WriteLine($"ID : {s1.Id}\nName : {s1.Name}\nEmail : {s1.Email}\nPhone : {s1.Phone}");
+        }
+    }
+}
---
+++ Apr062024/Apr062024/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+namespace Apr062024
+{
+    class Student
+    {
+        int _id;
+        string _name;
+        public string email;
+
+        public void set_id (int ID)
+        {
+            this._id = ID;
+        }
+        public void set_name(string Name)
+        {
+            this._name = Name;
+        }
+        public int get_id()
+        {
+            return this._id;
+        }
+        public string getName()
+        {
+            return this._name;
+        }
+
+    }
+    internal class Program
+    {
+        static void Main()
+        {
+            Student s1 = new Student();
+            s1.set_id (1);
+            s1.set_name("Ali Raza");
+            s1.email = "ali@raza.com";
+
+            Console.WriteLine($"ID : {s1.get_id()}\nName : {s1.getName()}\nEmail : {s1.email}");
+        }
+    }
+}
