fb218c520b827cf5b85a4d28661f70d701f13fdc
Syed Murtaza Hussain <sirmurtazaaptechtr@outlook.com>
2024-04-09 15:36:39 +0500

    09-Apr-2024-U

--- Apr092024/Apr092024/Program.cs
+++ Apr092024/Apr092024/Program.cs
@@ -5,28 +5,44 @@ namespace Apr092024
     class Employee
     {
 
-        public int _id;
+        private int _id;
         public string _name;
         public string _email;
         public string _phone;
+
+        public int Id {  get { return _id; } }
+
+        public Employee (int ID)
+        {
+            this._id = ID;
+        }
 
     }
     class MonthlyEmployee : Employee
     {
         public float _monthlysalary;
 
+        public MonthlyEmployee(int ID) : base(ID)
+        {
+        }
+
         public void Print()
         {
-            Console.WriteLine($"ID : {this._id}\nName : {this._name}\nEmail : {this._email}\nPhone : {this._phone}\nWeeklySalary : {this._monthlysalary / 30 * 7}");
+            Console.WriteLine($"ID : {this.Id}\nName : {this._name}\nEmail : {this._email}\nPhone : {this._phone}\nWeeklySalary : {this._monthlysalary / 30 * 7}");
         }
     }
 
     class ParttimeEmployee : Employee
     {
         public float _hourlyrate;
+
+        public ParttimeEmployee(int ID) : base(ID)
+        {
+        }
+
         public void Print()
         {
-            Console.WriteLine($"ID : {this._id}\nName : {this._name}\nEmail : {this._email}\nPhone : {this._phone}\nWeeklySalary : {this._hourlyrate * 8 * 7}");
+            Console.WriteLine($"ID : {this.Id}\nName : {this._name}\nEmail : {this._email}\nPhone : {this._phone}\nWeeklySalary : {this._hourlyrate * 8 * 7}");
         }
 
     }
@@ -35,15 +51,13 @@ namespace Apr092024
 
         static void Main()
         {
-            ParttimeEmployee pte1 = new ParttimeEmployee();
-            pte1._id = 1;
+            ParttimeEmployee pte1 = new ParttimeEmployee(1);            
             pte1._name = " Ali";
             pte1._email = " Ali@gmail.com";
             pte1._phone = "03121220006";
             pte1._hourlyrate = 1250.00f;
 
-            MonthlyEmployee me1 = new MonthlyEmployee();
-            me1._id = 2;
+            MonthlyEmployee me1 = new MonthlyEmployee(2);            
             me1._name = "hassan";
             me1._email = "hassan@gmail.com";
             me1._phone = "03333223231";
