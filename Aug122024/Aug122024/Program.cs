a0bbac1bb61c4b49ed17c1df9e76789a2195ad1d
Syed Murtaza Hussain <sirmurtazaaptechtr@outlook.com>
2024-08-16 20:56:30 +0500

    16-Aug-2024

---
+++ Aug122024/Aug122024/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Aug122024
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // This is a comment
+            /* The code below will print the words Hello World
+             * to the screen, and it is amazing */
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to CSharp");
+            Console.WriteLine("I am Learning C#");
+            Console.WriteLine("It is awesome!");
+            Console.WriteLine(3 + 7);
+            Console.Write("Aptech ");
+            Console.WriteLine("Learning");
+
+            string FullName = "Sadaf Irshad";
+            int age = 22;
+            Console.WriteLine("Dear " + FullName + ", you are " + age + " years old." );
+        }
+    }
+}
---
+++ Aug162024/Aug162024/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Aug162024
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // C# Variables
+            string name = "Javeria Shafique";
+            int age; // declaration statement
+            age = 18; // initilization statement
+            //double salary = 125455.25;
+            //float salary = 125455.25f;
+            decimal salary = 125455.25m;
+
+            // Composite format string
+            Console.WriteLine("Dear {1}, You are {0} years old.\nYour Salary is {2}", age, name, salary);
+            Console.WriteLine("Dear {0}, You are {1} years old.\nYour Salary is {2:C2}", name, age, salary);
+
+            //string concatenation
+            Console.WriteLine("Dear " + name + ", You are " + age + " years old.");
+
+            string[] names = { "Adam", "Bridgette", "Carla", "Daniel",
+                   "Ebenezer", "Francine", "George" };
+            decimal[] hours = { 40, 6.667m, 40.39m, 82,
+                    40.333m, 80, 16.75m };
+
+            Console.WriteLine("{0,-20} {1,5:N1}\n", "Name", "Hours");
+            for (int counter = 0; counter < names.Length; counter++)
+            {
+                Console.WriteLine("{0,-20} {1,5:N1}", names[counter], hours[counter]);
+            }
+            /*
+            This Console.WriteLine statement in C# is formatting and printing text with specific alignment and number formatting. Here's a breakdown of what each part does:
+            "{0,-20} {1,5:N1}\n": This is the format string.
+            {0,-20}: The first placeholder({ 0}) represents the first argument("Name"). The - 20 specifies left alignment within a 20 - character wide field.
+            { 1,5:N1}: The second placeholder({ 1}) represents the second argument("Hours"). The 5 specifies right alignment within a 5 - character wide field, and N1 formats the number with one decimal place and includes thousands separators if necessary.
+            \n: This is a newline character that moves the cursor to the next line after the output.
+            */
+
+        }
+    }
+}
