d9f2d790bd2aa6c5202832e4b93e9ec921f32c54
Syed Murtaza Hussain <sirmurtazaaptechtr@outlook.com>
2024-09-02 21:02:18 +0500

    Conditions

---
+++ Sep022024/Sep022024/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Sep022024
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            float maxmarks, obtmarks, percentage;
+            maxmarks = 75f;
+
+            Console.Write("Enter Obtained Marks : ");
+            obtmarks = float.Parse(Console.ReadLine());
+
+            percentage = (float)Math.Round(obtmarks / maxmarks * 100,2);
+
+            Console.WriteLine($"Max Marks : {maxmarks}\nObt. Marks : {obtmarks}\nPercentage : {percentage}%");
+            // for status
+            if(percentage < 40)
+            {
+                Console.WriteLine("Status : Fail");
+            }                      
+            else                   
+            {                      
+                Console.WriteLine("Status : Pass");
+            }
+            // for grade
+            if(percentage >= 80)
+            {
+                Console.WriteLine("Grade : A+");
+            }
+            else if (percentage >= 70)
+            {
+                Console.WriteLine("Grade : A");
+            }
+            else if (percentage >= 60)
+            {
+                Console.WriteLine("Grade : B");
+            }
+            else if (percentage >= 50)
+            {
+                Console.WriteLine("Grade : C");
+            }
+            else if (percentage >= 40)
+            {
+                Console.WriteLine("Grade : D");
+            }
+            else
+            {
+                Console.WriteLine("Grade : No-Grade");
+            }
+        }
+    }
+}
